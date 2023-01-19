401c4b22bae0168b1c092b5106c69cf670d703ed
woodenonesie <olga.pi4ugina@yandex.ru>
2023-01-18 17:40:20 -0800

    more changes

--- play.js
+++ play.js
@@ -1,6 +1,25 @@
 const net = require("net");
 // establishes a connection with the game server
-const {connect} = require("./client.js");
+const { connect } = require("./client.js");
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+
+  process.stdin.on('data', handleUserInput);
+
+  return stdin;
+};
+
+setupInput()
 
 console.log("Connecting ...");
 connect();
